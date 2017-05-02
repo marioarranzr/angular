1c4775f7a77c7676462c98ad5f779e04c24d230f
Mario Arranz <mario800ml@gmail.com>
2017-05-02 17:18:39 +0200

    pipes

---
+++ Angular/3. Pipes/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre = 'Mario';
+  array = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a:number = 0.234;
+  salario:number = 1234.5
+  heroe = {
+    nombre: "Logan",
+    clave:  "Wolverine",
+    edad:   500,
+    direccion: {
+      calle: "Calle Falsa",
+      numero: 123
+    }
+  }
+}
