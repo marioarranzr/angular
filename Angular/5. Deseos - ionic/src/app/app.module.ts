0dd60bee7ab2bbf398cd59c034828bb06ab27904
Mario Arranz <mario800ml@gmail.com>
2017-05-22 00:20:01 +0200

    deseos ionic app - listas pendientes y terminados

--- Angular/5. Deseos - ionic/src/app/app.module.ts
+++ Angular/5. Deseos - ionic/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { PendientesComponent} from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent} from '../pages/terminados/terminados.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,9 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PendientesComponent,
+    TerminadosComponent,
     TabsPage
   ],
   imports: [
@@ -26,9 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PendientesComponent,
+    TerminadosComponent,
     TabsPage
   ],
   providers: [
---
+++ Angular/5. Deseos - ionic/src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pendientes',
+  templateUrl: 'pendientes.component.html',
+})
+export class PendientesComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
