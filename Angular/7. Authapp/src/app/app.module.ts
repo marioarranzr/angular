bf04260331cfa734c72001342ab112b1db83a2b5
Mario Arranz <mario800ml@gmail.com>
2017-08-15 19:33:20 +0200

    configuración inicial, rutas y links de las 3 pantallas (home, precios y página protegida a la que daremos acceso mediante auth0)

--- Angular/7. Authapp/src/app/app.module.ts
+++ Angular/7. Authapp/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/7. Authapp/src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-protegida',
+  templateUrl: './protegida.component.html',
+  styles: []
+})
+export class ProtegidaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
